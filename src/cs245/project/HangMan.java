a4d3ff22efae3e57ebf29e081c4272353bd9a8d6
amnipp <amnipp@cpp.edu>
2017-01-12 18:36:31 -0800

    Added skeleton code to the base game

--- src/cs245/project/HangMan.java
+++ src/cs245/project/HangMan.java
@@ -6,13 +6,56 @@
 
 package cs245.project;
 
+import java.util.Random;
+
 /**
  *
  * @author amnipp
  */
 public class HangMan {
-    private String[] wordList;
+    private final String[] WORD_LIST = {"abstract", "cemetery", "nurse", "pharmacy", "climbing"};
+    private boolean running;
+    private String selectedWord;
+    private int attempt;
+    private int score;
+    
     public HangMan(){
-        wordList = new String[5];
+        initGame();
+        gameLoop();
+        endGame();
+    }
+    
+    public void initGame(){
+        running = true;
+        score = 100;
+        //Select a word
+        int randIndex = new Random().nextInt(WORD_LIST.length+1);
+        selectedWord = WORD_LIST[randIndex];
+    }
+    
+    private void gameLoop(){
+        while(running){
+            
+        }
+    }
+    
+    public void endGame(){
+        
+    }
+    
+    public char selectCharacter(){
+        return ' ';
+    }
+     
+    public boolean checkCharacter(char letter){
+        return selectedWord.contains(selectedWord);
+    }
+    
+    public String getSelectedWord(){
+        return selectedWord;
+    }
+    
+    public int getScore(){
+        return score;
     }
 }
