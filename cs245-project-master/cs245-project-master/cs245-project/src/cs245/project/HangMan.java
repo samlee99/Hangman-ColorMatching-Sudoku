96b294793d34724219c36d1fe580027a3e5b547d
jfcarter <jedon007@yahoo.com>
2017-01-15 12:13:19 -0800

    Add files via upload
    This has a clock, working Play button, working buttons on PlayScreen (no image changes yet)

---
+++ cs245-project-master/cs245-project-master/cs245-project/src/cs245/project/HangMan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs245.project;
+
+/**
+ *
+ * @author amnipp
+ */
+public class HangMan {
+    private String[] wordList;
+    public HangMan(){
+        wordList = new String[5];
+    }
+}
